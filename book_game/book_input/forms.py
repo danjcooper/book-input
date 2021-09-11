ed91583eb68cc68eaeba7eb1c5d707bd1291be17
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-09-11 21:05:48 +0100

    add placeholders for inputs

--- book_game/book_input/forms.py
+++ book_game/book_input/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from . import models
 
 
@@ -6,3 +7,6 @@ class regester_book_form(ModelForm):
     class Meta:
         model = models.Book
         fields = ['title', 'author', 'year', 'blurb', 'first_line', 'last_line', 'notes']
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Title'})
+        }
