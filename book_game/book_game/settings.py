e0f52bb7978e43366e875a295672d1ffe1987038
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-09-11 15:42:45 +0100

    Add deployed database

--- book_game/book_game/settings.py
+++ book_game/book_game/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,10 +78,21 @@ WSGI_APPLICATION = 'book_game.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("HEROKU_DB_NAME"),
+        'USER': os.environ.get("HEROKU_DB_USER"),
+        'PASSWORD': os.environ.get("HEROKU_DB_PASSWORD"),
+        'HOST': os.environ.get("HEROKU_DB_HOST"),
+        'PORT':  os.environ.get("HEROKU_DB_PORT"),
     }
 }
 
