0c4de1e0136f0982d7ccd6d43096d7da44d467e6
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-09-11 21:38:53 +0100

    start styling

--- book_game/book_input/forms.py
+++ book_game/book_input/forms.py
@@ -8,5 +8,11 @@ class regester_book_form(ModelForm):
         model = models.Book
         fields = ['title', 'author', 'year', 'blurb', 'first_line', 'last_line', 'notes']
         widgets = {
-            'title': forms.TextInput(attrs={'placeholder': 'Title'})
+            'title': forms.TextInput(attrs={'placeholder': 'Title'}),
+            'author': forms.TextInput(attrs={'placeholder': 'Author'}),
+            'year': forms.NumberInput(attrs={'placeholder': 'Year'}),
+            'blurb': forms.Textarea(attrs={'placeholder': 'Blurb'}),
+            'first_line': forms.TextInput(attrs={'placeholder': 'First line'}),
+            'last_line': forms.TextInput(attrs={'placeholder': 'Last line'}),
+            'notes': forms.TextInput(attrs={'placeholder': 'Notes'}),
         }
