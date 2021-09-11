69f2764454d878f216c4ce5c0f8dc105025be506
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-09-11 15:01:27 +0100

    Change allowed hosts

--- book_game/book_game/settings.py
+++ book_game/book_game/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jqc^(!aq3@9&^h@zf#amr9z)3$3p!x%5de3m!d*c-dn_85=00s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
